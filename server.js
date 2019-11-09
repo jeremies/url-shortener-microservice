02da7e2e468d6a45f2723360803de941aad2cdfc
Glitch (sepia-juniper) <none>
2019-11-09 10:58:13 +0000

    🍓🚓 Checkpoint ./server.js:987705/226

--- server.js
+++ server.js
@@ -24,11 +24,18 @@ app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+
+var Schema = mongoose.Schema;
+
+var URLSchema = new Schema({
+  id:  Number,
+  url: String
+});
+
 
 
 app.listen(port, function () {
